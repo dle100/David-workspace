19fcccac5a2d3ffc5e08ebc875d11cd8eaa2e7fd
avias8 <HarshvardhanV98@gmail.com>
2021-08-01 18:32:20 -0600

    Final Version

--- COSC 304 Lab 6/EnrollDB.py
+++ COSC 304 Lab 6/EnrollDB.py
@@ -329,92 +329,92 @@ class EnrollDB:
 #################################################################
 
 
-# # Main execution for testing
-# enrollDB = EnrollDB()
-# enrollDB.connect()
+# Main execution for testing
+enrollDB = EnrollDB()
+enrollDB.connect()
 
-# # # Prevent rebuilding on each try of the code.
-# enrollDB.init()
-# # #Question 1 Part 1:
-# results = (enrollDB.listAllStudents())
-# print(results)
+# # Prevent rebuilding on each try of the code.
+enrollDB.init()
+# #Question 1 Part 1:
+results = (enrollDB.listAllStudents())
+print(results)
 
-# # #Question 1 Part 2:
-# print("Executing list professors in a department: Computer Science")
-# print(enrollDB.listDeptProfessors("Computer Science"))
-# print("Executing list professors in a department: none")
-# print(enrollDB.listDeptProfessors("none"))
+# #Question 1 Part 2:
+print("Executing list professors in a department: Computer Science")
+print(enrollDB.listDeptProfessors("Computer Science"))
+print("Executing list professors in a department: none")
+print(enrollDB.listDeptProfessors("none"))
 
-# # Question 1 Part 3:
-# print("Executing list students in course: COSC 304")
-# output = enrollDB.listCourseStudents("COSC 304")
-# print(output)
+# Question 1 Part 3:
+print("Executing list students in course: COSC 304")
+output = enrollDB.listCourseStudents("COSC 304")
+print(output)
 
-# print("Executing list students in course: DATA 301")
-# output = enrollDB.listCourseStudents("DATA 301")
-# print(output)
+print("Executing list students in course: DATA 301")
+output = enrollDB.listCourseStudents("DATA 301")
+print(output)
 
-# # Question 1 Part 4:
-# print("Executing compute GPA for student: 45671234")
-# print(enrollDB.resultSetToString(enrollDB.computeGPA("45671234"), 10))
+# Question 1 Part 4:
+print("Executing compute GPA for student: 45671234")
+print(enrollDB.resultSetToString(enrollDB.computeGPA("45671234"), 10))
 
-# print("Executing compute GPA for student: 00000000")
-# enrollDB.resultSetToString(enrollDB.computeGPA("45671234"), 10)
+print("Executing compute GPA for student: 00000000")
+enrollDB.resultSetToString(enrollDB.computeGPA("45671234"), 10)
 
-# # Question 1 Part 5
-# enrollDB.init()
-# print("Adding student 55555555:")
-# enrollDB.addStudent("55555555",  "Stacy Smith", "F", "1998-01-01")
-# print("Adding student 11223344:")
-# enrollDB.addStudent("11223344",  "Jim Jones", "M",  "1997-12-31")
+# Question 1 Part 5
+enrollDB.init()
+print("Adding student 55555555:")
+enrollDB.addStudent("55555555",  "Stacy Smith", "F", "1998-01-01")
+print("Adding student 11223344:")
+enrollDB.addStudent("11223344",  "Jim Jones", "M",  "1997-12-31")
 
-# # Question 1 Part 6
-# enrollDB.init()
-# print("Test delete student:")
-# print("Deleting student 99999999:")
-# enrollDB.deleteStudent("99999999")
-# # Non-existing student
-# print("Deleting student 00000000:")
-# enrollDB.deleteStudent("00000000")
+# Question 1 Part 6
+enrollDB.init()
+print("Test delete student:")
+print("Deleting student 99999999:")
+enrollDB.deleteStudent("99999999")
+# Non-existing student
+print("Deleting student 00000000:")
+enrollDB.deleteStudent("00000000")
 
 
-# # Question 1 Part 7
-# enrollDB.init()
-# print("Updating student 99999999:")
-# enrollDB.updateStudent("99999999",  "Wang Wong", "F", "1995-11-08", 3.23)
-# print("Updating student 00567454:")
-# enrollDB.updateStudent("00567454",  "Scott Brown", "M",  None, 4.00)
+# Question 1 Part 7
+enrollDB.init()
+print("Updating student 99999999:")
+enrollDB.updateStudent("99999999",  "Wang Wong", "F", "1995-11-08", 3.23)
+print("Updating student 00567454:")
+enrollDB.updateStudent("00567454",  "Scott Brown", "M",  None, 4.00)
 
-# # Question 1 part 8
-# enrollDB.init()
-# print("Test new enrollment in COSC 304 for 98123434:")
-# # Add an enroll with a student
-# enrollDB.newEnroll("98123434", "COSC 304", "001", 2.51)
+# Question 1 part 8
+enrollDB.init()
+print("Test new enrollment in COSC 304 for 98123434:")
+# Add an enroll with a student
+enrollDB.newEnroll("98123434", "COSC 304", "001", 2.51)
 
-# # Question 1 part 9
-# enrollDB.init()
-# print("Test update student GPA for student:")
-# enrollDB.newEnroll("98123434", "COSC 304", "001", 3.97)
-# enrollDB.updateStudentGPA("98123434")
+# Question 1 part 9
+enrollDB.init()
+print("Test update student GPA for student:")
+enrollDB.newEnroll("98123434", "COSC 304", "001", 3.97)
+enrollDB.updateStudentGPA("98123434")
 
-# # Question 1 part 10
-# print("Test update student mark for student 98123434 to 3.55:")
-# enrollDB.updateStudentMark("98123434", "COSC 304", "001", 3.55)
+# Question 1 part 10
+print("Test update student mark for student 98123434 to 3.55:")
+enrollDB.updateStudentMark("98123434", "COSC 304", "001", 3.55)
 
-# # Question 1 part 11
-# enrollDB.init()
-# enrollDB.removeStudentFromSection("00546343", "CHEM 113", "002")
+# Question 1 part 11
+enrollDB.init()
+enrollDB.removeStudentFromSection("00546343", "CHEM 113", "002")
 
-# # Queries
-# # Re-initialize all data
+# Queries
+# Re-initialize all data
 
-# enrollDB.init()
-# print(enrollDB.resultSetToString(enrollDB.query1(), 100))
+enrollDB.init()
+print(enrollDB.resultSetToString(enrollDB.query1(), 100))
 
-# print(enrollDB.resultSetToString(enrollDB.query2(), 100))
+print(enrollDB.resultSetToString(enrollDB.query2(), 100))
 
-# print(enrollDB.resultSetToString(enrollDB.query3(), 100))
+print(enrollDB.resultSetToString(enrollDB.query3(), 100))
 
-# print(enrollDB.resultSetToString(enrollDB.query4(), 100))
+print(enrollDB.resultSetToString(enrollDB.query4(), 100))
 
-# enrollDB.close()
+enrollDB.close()
